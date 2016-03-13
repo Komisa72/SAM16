ad9aac43d3c1d5ae46857c1ae04e63541fe69557
Komisa72 <a.m.net@gmx.net>
2016-03-13 19:34:18 +0100

    This is a Request Scoped Backing bean, can be used e. g. for creation of new user, or buiyng shares.

---
+++ trunk/ds-finance-bank-web/src/main/java/at/technikum/bicss/sam/trading/CustomerController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.technikum.bicss.sam.trading;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ */
+@RequestScoped
+public class CustomerController {
+
+    /**
+     * Creates a new instance of CustomerController
+     */
+    public CustomerController() {
+    }
+    
+}
