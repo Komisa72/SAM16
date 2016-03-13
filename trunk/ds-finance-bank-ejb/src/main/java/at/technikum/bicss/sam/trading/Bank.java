816655d23bae2174e8b1de520dcccf91bcde76c6
Komisa72 <a.m.net@gmx.net>
2016-03-13 18:59:56 +0100

    added skeleton of Local EJB with stub mehtode sayHello

---
+++ trunk/ds-finance-bank-ejb/src/main/java/at/technikum/bicss/sam/trading/Bank.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.technikum.bicss.sam.trading;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+/**
+ * @author amaierhofer
+ */
+@Stateless(name = "BankEJB", mappedName = "EJB-Model-BankEJB")
+@Local(BankInterface.class)
+public class Bank implements BankInterface {
+
+    /** sayHello
+     * TODO to be replaced by real methods from BankInterface
+     *
+     */
+    @Override
+    public void sayHello() {
+        // TODO subject to change, remove unused code.
+        int i = 0;
+        i = 5;
+    }
+
+
+}
---
+++ trunk/ds-finance-bank-ejb/src/main/java/at/technikum/bicss/sam/trading/BankInterface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.technikum.bicss.sam.trading;
+
+import javax.ejb.Local;
+
+/** BankInterface
+ * Local Interface consumed by web applicaton.
+ */
+@Local
+public interface BankInterface {
+    
+    /** sayHello
+     * TP
+     * TODO add the real interface methods used by the web application.
+     */
+    public void sayHello();
+}
